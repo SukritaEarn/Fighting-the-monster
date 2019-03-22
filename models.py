b271c3047111518096e9f60092b4e7d758cee8f9
SukritaEarn <sukrita.ki@ku.th>
2019-03-22 15:49:51 +0700

    add pictures

---
+++ models.py
@@ -0,0 +1,19 @@
+import arcade
+
+class Warrior:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+    
+    def update(self):
+        pass
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.warrior = Warrior(self, 60, 100)
+    
+    def update(self, delta):
+        self.warrior.update(delta)
