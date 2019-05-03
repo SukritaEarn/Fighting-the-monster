4c160dfb04c45848e85e624fff4606b4e6c01ad6
SukritaEarn <sukrita.ki@ku.th>
2019-05-03 09:32:44 +0700

    reduce hp

--- game.py
+++ game.py
@@ -33,26 +33,32 @@ class GameWindow(arcade.Window):
                                         model=self.world.war_wea)
         self.mon_wea_sprite = ModelSprite('images/mon_wea.png',
                                         model=self.world.mon_wea)
-        self.war_hp_sprite = ModelSprite('images/war_hp.png',
-                                        model=self.world.war_hp)
-        self.mon_hp_sprite = ModelSprite('images/mon_hp.png',
-                                        model=self.world.mon_hp)
-
-    def hit_mons(self):
-        hit = arcade.check_for_collision(self.war_wea_sprite, self.monster_sprite)
-        if hit:
-            print('hit')
-                
+        self.war_icon_sprite = arcade.Sprite('images/war_icon.png', 1,
+                                            center_x = 450, center_y = 530)
+        self.mon_icon_sprite = arcade.Sprite('images/mon_icon.png', 1,
+                                            center_x = 550, center_y = 530)
+        self.sand_clock_sprite = arcade.Sprite('images/sand_clock.png', 0.3,
+                                            center_x = 850, center_y = 300)
+        self.power_sprite = Power(160, 310, 160, 310, 10)
+        self.check_press = None
+        self.reduce_mon_hp = Reduce_Mon_HP(910, 530, 30, 22)
+        self.reduce_war_hp = Reduce_Mon_HP(90, 530, 30, 22)
+    
     def on_mouse_press(self, x, y, button, modifiers):
         self.world.on_mouse_press(x, y, button, modifiers)
-
+        self.power_sprite = Power(160, 310, 160, 310, 10)
+        self.check_press = 1
+        
     def on_mouse_release(self, x, y, button, modifiers):
         if not self.world.is_started():
             self.world.start()
         self.world.on_mouse_release(x, y, button, modifiers)
+        self.check_press = 2
 
     def update(self, delta):
         self.world.update(delta)
+        if self.check_press == 1:
+            self.power_sprite.update(delta)
 
     def on_draw(self):
         arcade.start_render()
@@ -64,19 +70,89 @@ class GameWindow(arcade.Window):
         self.mon_wea_sprite.draw()
         self.warrior_sprite.draw()
         self.monster_sprite.draw()
-        
+
         arcade.draw_rectangle_filled(250, 530, 360, 32, arcade.color.BLACK_BEAN)
         arcade.draw_circle_filled(450, 530, 33, arcade.color.BLACK_BEAN)
         arcade.draw_rectangle_filled(250, 530, 350, 22, arcade.color.RED_BROWN)
         arcade.draw_circle_filled(450, 530, 28, arcade.color.RED_BROWN)
-        self.war_hp_sprite.draw()
+        self.war_icon_sprite.draw()
         
         arcade.draw_rectangle_filled(750, 530, 360, 32, arcade.color.BLACK_BEAN)
         arcade.draw_circle_filled(550, 530, 33, arcade.color.BLACK_BEAN)
         arcade.draw_rectangle_filled(750, 530, 350, 22, arcade.color.RED_BROWN)
         arcade.draw_circle_filled(550, 530, 28, arcade.color.RED_BROWN)
-        self.mon_hp_sprite.draw()
+        self.mon_icon_sprite.draw()
+        
+        if self.check_press == 1:
+            self.power_sprite.draw()
 
+        if self.check_press == 2:
+            self.power_sprite.remove()
+
+        if self.world.war_wea.check_hit() in [1,2,3]:
+            self.reduce_mon_hp.draw()
+
+        if self.world.war_wea.check_hit() in [4,5]:
+            self.reduce_mon_hp.miss()
+
+        if self.world.mon_wea.check_hit() in [1,2,3]:
+            self.reduce_war_hp.draw()
+
+        if self.world.mon_wea.check_hit() in [4,5]:
+            self.reduce_war_hp.miss()
+
+class Reduce_Mon_HP:
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, arcade.color.WHITE)
+
+    def miss(self):
+        arcade.Sprite('images/miss.png', 0.5, center_x = 850, center_y = 300)
+
+    def update(self, delta):
+        self.width -= 10
+
+class Reduce_War_HP:
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, arcade.color.WHITE)
+
+    def miss(self):
+        arcade.Sprite('images/miss.png', 0.5, center_x = 120, center_y = 300)
+
+    def update(self, delta):
+        self.width += 10
+
+class Power:
+
+    def __init__(self, x1, y1, x2, y2, width):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.width = width
+
+    def draw(self):
+        arcade.draw_line(self.x1, self.y1, self.x2, self.y2, arcade.color.RED, self.width)
+
+    def remove(self):
+        self.width = 0
+
+    def update(self, delta):
+        self.x2 -= 1
+        
 def main():
     window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.set_window(window)
--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 import arcade
-import math
 import time
 import random
 
 class Warrior:
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
@@ -14,6 +14,7 @@ class Warrior:
         pass
 
 class Monster:
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
@@ -24,20 +25,26 @@ class Monster:
         pass
 
 class War_HP:
-    def __init__(self, world, x, y):
+
+    def __init__(self, world, x, y, width, height):
         self.world = world
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
         self.angle = 0
         
     def update(self, delta):
         pass
 
 class Mon_HP:
-    def __init__(self, world, x, y):
+
+    def __init__(self, world, x, y, width, height):
         self.world = world
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
         self.angle = 0
         
     def update(self, delta):
@@ -56,28 +63,28 @@ class War_Wea:
         self.vx = War_Wea.VELOCITY_X
         self.vy = War_Wea.VELOCITY_Y
         self.angle = 0
-        self.war_wea_list = []
 
     def throw(self, start, end):
-        #self.vy = (World.END_TIME - World.START_TIME)*5
-        self.vy = (end - start)*5
+        if end - start >= 5:
+            start = 0
+            end = 0
+        self.vy = (end - start)*1.5
 
-    def check_hit(self, mon):
-        if 320 <= self.y <= 340 and self.x == 850:
+    def check_hit(self):
+        if 320 <= self.y <= 340 and self.x >= 850:
             return 1
-        elif 171 <= self.y <= 319 and self.x == 850:
+        elif 171 <= self.y <= 319 and self.x >= 850:
             return 2
-        elif 120 <= self.y <= 170 and self.x == 850:
+        elif 120 <= self.y <= 170 and self.x >= 850:
             return 3
-        elif self.y >= 341 and self.x == 850:
+        elif self.y >= 341 and self.x >= 850:
             return 4
-        elif self.y <= 119 and self.x == 850:
+        elif self.y <= 119 and self.x >= 850:
             return 5
 
-    def kill(self, x, y, angle):
-        self.x = x
-        self.y = y
-        self.angle = angle
+    def kill(self):
+        self.x = 120
+        self.y = 250
 
     def update(self, delta):
         self.x += self.vx
@@ -88,8 +95,9 @@ class War_Wea:
 class Mon_Wea:
 
     VELOCITY_X = 10
-    VELOCITY_Y = random.randint(2,7)
+    VELOCITY_Y = random.randint(1,6)
     GRAVITY = 0.088
+    THROW_WAIT = 1.5
     
     def __init__(self, world, x, y):
         self.world = world
@@ -98,8 +106,28 @@ class Mon_Wea:
         self.vx = Mon_Wea.VELOCITY_X
         self.vy = Mon_Wea.VELOCITY_Y
         self.angle = 0
+        self.wait_time = 0
+
+    def check_hit(self):
+        if 290 <= self.y <= 318 and self.x <= 120:
+            return 1
+        elif 133 <= self.y <= 289 and self.x <= 120:
+            return 2
+        elif 102 <= self.y <= 132 and self.x <= 120:
+            return 3
+        elif self.y >= 319 and self.x <= 120:
+            return 4
+        elif self.y <= 101 and self.x <= 120:
+            return 5
+
+    def kill(self):
+        self.x = 120
+        self.y = 210
 
     def update(self, delta):
+        self.wait_time += delta
+        if self.wait_time < Mon_Wea.THROW_WAIT:
+            return
         self.x -= self.vx
         self.y += self.vy
         self.vy -= Mon_Wea.GRAVITY
@@ -121,8 +149,8 @@ class World:
         self.monster = Monster(self, 850, 230)
         self.war_wea = War_Wea(self, 120, 250)
         self.mon_wea = Mon_Wea(self, 850, 250)
-        self.war_hp = War_HP(self, 450, 530)
-        self.mon_hp = Mon_HP(self, 550, 530)
+        self.war_hp = War_HP(self, 450, 530, 0, 0)
+        self.mon_hp = Mon_HP(self, 550, 530, 0 ,0)
 
     def start(self):
         self.state = World.STATE_STARTED
@@ -158,17 +186,7 @@ class World:
         if self.state == World.STATE_STARTED:
             self.war_wea.update(delta)
         if self.war_wea.x >= self.monster.x:
-            #if self.war_wea.check_hit(self.monster) == 1:
-                #print('headshot')
-            #if self.war_wea.check_hit(self.monster) == 2:
-                #print('bodyhit')
-            #if self.war_wea.check_hit(self.monster) == 3:
-                #print('scrape')
-            #else:
-                #print('miss')
             self.mon_wea.update(delta)
-            
         if self.mon_wea.x <= -35:
             self.state = World.STATE_FROZEN
-            print((World.END_TIME - World.START_TIME) * 5)
             self.reset()
